18c8269a5fefcf9abf89fb156441ebc1aed21b6a
Mickaël Mao <mickael@commerceguys.com>
2013-07-29 17:30:03 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sharemusic::Application.config.secret_key_base = '31fc11e6395e532edaca8e6c5a351ca37586edfd6416990a3efb66c6252154158ea20137ee4e983ffb7db808586bb772ac6b01d0689a7e142fe07803ebbad932'
